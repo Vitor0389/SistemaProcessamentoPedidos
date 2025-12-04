f2a6c1a158d2e409b68bb9677f63253c6f86f228
Vitor0389 <vitor0389@gmail.com>
2025-12-03 21:16:28 -0300

    chore: iniciando servico-estoque

---
+++ servico-estoque/src/test/java/com/arquitetura/servico_estoque/ServicoEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arquitetura.servico_estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
